67a5e25ff0211ebe5590585b0f51c2d3f6e71448
Scott Epple <smepple@gmail.com>
2013-02-23 14:33:35 -0500

    fix assets:precompile failure with heroku

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,8 @@ module FoodTracker
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.initialize_on_precompile = false
+
     # Filepicker API config
     config.filepicker_rails.api_key = 'Ax3lLqeeITrS7ZGTJhKF1z'
   end
